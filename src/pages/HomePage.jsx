26aa7384f38c90ad82e98b957d7488bdea9ad842
Oamogetswese <Oamogetswese@gmail.com>
2024-12-04 23:52:23 +0200

    new style

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
-
+import '../styles/HomePageS'
 
 // HomePage component: Fetches and displays a list of shows
 const HomePage = () => {
